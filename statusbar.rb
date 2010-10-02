38f42bf7cacc987b24e848a77a7e04da564756d1
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2010-10-02 19:29:57 +0200

    introduced thread-based info-refresh

--- statusbar.rb
+++ statusbar.rb
@@ -6,6 +6,10 @@ BAR_WIDTH=70
 
 class Widget
 
+	def initialize(interval)
+		Thread.new { loop { refresh_info; sleep interval } }
+	end
+
 	def name
 		return "default widget"
 	end
@@ -18,13 +22,10 @@ end
 
 class Battery < Widget
 	
-	def initialize
+	def initialize(interval)
 		@PREFIX = "/sys/class/power_supply/"
-		@BAT_SIZE = 20
-		@BAT_FULL_CHAR = "#"
-		@BAT_LOW_CHAR= "-"
 		
-		refresh_info
+		super
 	end
 
 	def name
@@ -32,22 +33,16 @@ class Battery < Widget
 	end
 
 	def to_s
-		refresh_info
-
 		bat_perc = @charge_now.to_f/@charge_full*100
-		bat_full_num = (@BAT_SIZE * bat_perc / 100).to_i
 
 		"^i(#{ICON_BASE}/bat_empty_01.xbm)^fg(" << ac_status_color << ")" << " (ac)^fg()" <<
-		#"^fg(blue)%[^fg(green)" << @BAT_FULL_CHAR * bat_full_num << "^fg()" <<
-		#"^fg(red)" << @BAT_LOW_CHAR * (@BAT_SIZE-bat_full_num) << "^fg(blue)]^fg()" <<
-		#"^fg(blue)" << bat_perc.to_s.rjust(4) << "^fg()"
 		`echo #{bat_perc} | gdbar -fg 'green' -bg '#494b4f' -h #{BAR_HEIGHT} -w #{BAR_WIDTH}`
 	end
 
 
 	private
 
-	def refresh_info		
+	def refresh_info	
 		@bat_status = `cat #{@PREFIX}/CMB1/status`.chomp.downcase
 		@charge_full = `cat #{@PREFIX}/CMB1/charge_full`.chomp.to_i
 		@charge_now = `cat #{@PREFIX}/CMB1/charge_now`.chomp.to_i
@@ -68,32 +63,56 @@ class Clock < Widget
 	end
 
 	def to_s
-		`date +'^fg(darkgrey)%a %d.%m ^fg(lightblue)%H:%M^fg()'`.chomp
+		"^fg(darkgrey)#{@time.day.to_s.rjust(2,"0")}.#{@time.mon.to_s.rjust(2,"0")}^fg()" <<
+		" ^fg(lightblue)#{@time.hour.to_s.rjust(2,"0")}:#{@time.min.to_s.rjust(2,"0")}^fg()"
+	end
+
+
+	private
+
+	def refresh_info
+		@time = Time.now
 	end
 
 end
 
 
 class Cpu < Widget
+# TODO
+# don't use gcpubar (ugly style)
+# make use of refresh_info
 
 	def name
 		"Cpu-Usage"
 	end
 
 	def to_s
+
 		str = `gcpubar -c 2 -i 0.1 -fg 'blue' -bg '#494b4f' -h #{BAR_HEIGHT} -w #{BAR_WIDTH}`
 		str = str.split("\n")[1].chomp
 		str.slice!(0..4)
 		"^i(#{ICON_BASE}/fs_01.xbm) " << str
 	end
 
+	private
+
+	def refresh_info
+		
+	end
+
 end
 
 
 class Mpd < Widget
+# TODO
+# remove mpc dependency and use mpd-lib instead
+# make use of refresh_info
 
-	def initialize
+	def initialize(interval,interval_notav)
 		@not_av_counter = 0 	# to make less queries if not available
+		@interval_notav = interval_notav
+
+		super(interval)
 	end
 
 	def name
@@ -104,7 +123,7 @@ class Mpd < Widget
 		str = ""
 		str = `mpc -h 192.168.0.1 current 2> /dev/null` if @not_av_counter == 0
 		if str == ""
-			@not_av_counter = 30 if @not_av_counter == 0
+			@not_av_counter = @interval_notav if @not_av_counter == 0
 			
 			str = "info not available"
 			@not_av_counter -= 1
@@ -112,7 +131,11 @@ class Mpd < Widget
 			@not_av_counter = 0
 		end
 
-		"^i(#{ICON_BASE}/note.xbm) " << str.ljust(50)
+		"^i(#{ICON_BASE}/note.xbm) " << str.chomp!.ljust(50)
+	end
+
+	def refresh_info
+
 	end
 end
 
@@ -120,14 +143,14 @@ end
 
 INTERVAL=2	# in seconds
 
-bat=Battery.new
-clock=Clock.new
-cpu=Cpu.new
-mpd=Mpd.new
+bat=Battery.new(3)
+clock=Clock.new(1)
+cpu=Cpu.new(3)
+mpd=Mpd.new(3,30)
 
 loop do
 	puts (clock.to_s << " | " << mpd.to_s << " ^fg(grey)| ^r(600x2) |^fg() " <<
 		cpu.to_s << " | " << bat.to_s)
 	STDOUT.flush
-	sleep INTERVAL	
+	sleep INTERVAL
 end
