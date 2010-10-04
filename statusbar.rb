7c2ec0482cb5db36c2c8804fad4f387731de935e
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2010-10-04 18:18:10 +0200

    let the ruby-script manage dzen

--- statusbar.rb
+++ statusbar.rb
@@ -143,18 +143,28 @@ end
 
 INTERVAL=2	# in seconds
 
+
+# initialize widgets
 bat=Battery.new(3)
 clock=Clock.new(1)
 cpu=Cpu.new(3)
 mpd=Mpd.new
 
-loop do
-	puts (clock.to_s << " | " << mpd.to_s << " ^fg(grey)| ^r(600x2) |^fg() " <<
-		cpu.to_s << " | " << bat.to_s)
-	STDOUT.flush
-	sleep INTERVAL
+# get screen resolution(s)
+# xrandr | grep '*'
 
-	trap "INT" do
-		mpd.close
+IO.popen("dzen2","w+") do |f|
+
+	loop do
+
+		f.write(clock.to_s << " | " << mpd.to_s << " ^fg(grey)| ^r(600x2) |^fg() " <<
+			cpu.to_s << " | " << bat.to_s)
+		f.flush
+		sleep INTERVAL
+
+		trap "INT" do
+			mpd.close
+		end
 	end
+
 end
